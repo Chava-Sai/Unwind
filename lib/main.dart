107ffbd51bb8d34569016e163859f9c3ce62f239
Chava-Sai <srinivassaichava@gmail.com>
2023-03-27 23:08:21 +0530

    Scroll Page update

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: 'login',
+      initialRoute: 'scroll',
       routes: {
         'login' : (context) => LoginPage(),
         'register' : (context) => MyRegister(),
--- lib/scroll.dart
+++ lib/scroll.dart
@@ -1,50 +1,108 @@
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
-class ScrollPage extends StatelessWidget {
+class ScrollPage extends StatefulWidget {
+  @override
+  _ScrollPageState createState() => _ScrollPageState();
+}
+
+class _ScrollPageState extends State<ScrollPage> {
+  double _opacity = 1.0;
+
   @override
   Widget build(BuildContext context) {
-    final panelHeightClosed = MediaQuery.of(context).size.height*0.05;
-    final panelHeightOpen = MediaQuery.of(context).size.height*0.8;
-    return Scaffold(
-      body: SlidingUpPanel(
-        minHeight: panelHeightClosed,
-        maxHeight: panelHeightOpen,
-        color: Colors.white.withOpacity(0.9),
-        panelBuilder: (scrollController) => _buildPanel(scrollController),
-        borderRadius: BorderRadius.vertical(top: Radius.circular(18)),
-        parallaxEnabled: true,
-        parallaxOffset: .5,
+    final panelHeightClosed = MediaQuery.of(context).size.height * 0.05;
+    final panelHeightOpen = MediaQuery.of(context).size.height * 1.0;
+
+    return NotificationListener<ScrollNotification>(
+      onNotification: (notification) {
+        setState(() {
+          _opacity = 1.0 - notification.metrics.pixels / 200.0;
+        });
+        return true;
+      },
+      child: Stack(
+        children: [
+          Opacity(
+            opacity: _opacity,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('Assets/image/back.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            body: SlidingUpPanel(
+              minHeight: panelHeightClosed,
+              maxHeight: panelHeightOpen,
+              color: Colors.transparent,
+              panelBuilder: (scrollController) =>
+                  _buildPanel(scrollController),
+              borderRadius: BorderRadius.vertical(top: Radius.circular(18)),
+              parallaxEnabled: true,
+              parallaxOffset: .5,
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  Widget _buildPanel(ScrollController scrollController) => ListView(
-    padding: EdgeInsets.zero,
-    children: [
-      SizedBox(height: 16,),
-      buildAboutText(),
-      SizedBox(height: 24,),
-    ],
+  Widget _buildPanel(ScrollController scrollController) => Container(
+    padding: EdgeInsets.symmetric(horizontal: 24),
+    child: CustomScrollView(
+      controller: scrollController,
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 16),
+              buildHeader(),
+              SizedBox(height: 18),
+            ],
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+              return Padding(
+                padding: EdgeInsets.only(left: 10, bottom: 20, right: 10),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.withOpacity(0.3),
+                  ),
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  width: MediaQuery.of(context).size.width,
+                ),
+              );
+            },
+            childCount: 4,
+          ),
+        ),
+      ],
+    ),
   );
 
- Widget buildAboutText() => Container(
-   padding: EdgeInsets.symmetric(horizontal: 24),
-   child: Column(
-     crossAxisAlignment: CrossAxisAlignment.start,
-     children: [
-       Center(
-         child: InkWell(
-           onTap: () => {},
-           child: Icon(
-             Icons.keyboard_arrow_up,
-             size: 32,
-           ),
-         ),
-       ),
-       SizedBox(height: 10,),
-       Text('ChatGPT is a large language model developed by OpenAI based on the GPT (Generative Pretrained Transformer) architecture. It is designed to generate human-like responses to text input, making it an effective tool for natural language processing (NLP) tasks such as language translation, text summarization, and conversational AI.ChatGPT has been trained on a massive corpus of text data, including books, articles, and web pages, which allows it to generate high-quality, coherent responses to a wide range of input queries. It uses deep learning techniques such as attention mechanisms and transformers to analyze and generate text, making it highly effective at generating natural-sounding, contextually-appropriate responses.ChatGPT is used in a variety of applications, including chatbots, virtual assistants, and customer service systems. It has the potential to revolutionize the way people interact with technology by making it easier and more natural to communicate with computers and other devices.'),
-     ],
-   ),
- );
+  Widget buildHeader() => Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Center(
+        child: InkWell(
+          onTap: () => {},
+          child: Icon(
+            Icons.keyboard_arrow_up,
+            size: 32,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      SizedBox(height: 10),
+    ],
+  );
 }

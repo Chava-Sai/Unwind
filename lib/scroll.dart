fe07164f6d59c32830b549ab302c65a70b79f5e6
Chava-Sai <srinivassaichava@gmail.com>
2023-03-28 01:52:47 +0530

    Scroll Page and widgets added

--- lib/scroll.dart
+++ lib/scroll.dart
@@ -68,22 +68,98 @@ class _ScrollPageState extends State<ScrollPage> {
             ),
             SliverList(
               delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  return Padding(
-                    padding: EdgeInsets.only(left: 10, bottom: 20, right: 10),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey.withOpacity(0.3),
+                    (BuildContext context, int index) {
+                  if (index == 0) {
+                    return GestureDetector(
+                      onTap: () {Navigator.pushNamed(context, 'chat');},
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 10, bottom: 20, right: 10),
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.2,
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.grey.withOpacity(0.3),
+                          ),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex: 5,
+                                child: Container(
+                                  width: MediaQuery.of(context).size.width * 0.3,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(10),
+                                      bottomLeft: Radius.circular(10),
+                                    ),
+                                    image: DecorationImage(
+                                      image: AssetImage('Assets/image/chatbot-1.png'),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 4,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(
+                                      topRight: Radius.circular(10),
+                                      bottomRight: Radius.circular(10),
+                                    ),
+                                    image: DecorationImage(
+                                      image: AssetImage('Assets/image/chatbot-2.png'),
+                                      fit: BoxFit.cover,
+                                      colorFilter: ColorFilter.mode(
+                                        Colors.white.withOpacity(0.9),
+                                        BlendMode.dstATop,
+                                      ),
+                                    ),
+                                  ),
+                                  width: MediaQuery.of(context).size.width * 0.7,
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left: 28),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          'CHAT',
+                                          style: TextStyle(
+                                            fontSize: 40,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                      height: MediaQuery.of(context).size.height * 0.2,
-                      width: MediaQuery.of(context).size.width,
-                    ),
-                  );
+                    );
+                  } else {
+                    return Padding(
+                      padding: EdgeInsets.only(left: 10, bottom: 20, right: 10),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.grey.withOpacity(0.3),
+                        ),
+                        height: MediaQuery.of(context).size.height * 0.2,
+                        width: MediaQuery.of(context).size.width,
+                      ),
+                    );
+                  }
                 },
                 childCount: 4,
               ),
-            ),
+            )
+            ,
+
+
           ],
         ),
       );
